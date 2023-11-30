32acc4dc049df05e14e0ef824763eadd4a61dee1
Shweta-1030 <140487195+Shweta-1030@users.noreply.github.com>
2023-11-30 16:02:05 +0530

    Add files via upload

--- detectika/Home.py
+++ detectika/Home.py
@@ -3,9 +3,8 @@ import streamlit as st
 st.set_page_config(layout="wide")
 
 st.title('Documentation📖')
-
 st.subheader('DETECTIKA DOC🔍')
-
 st.subheader('SENTILYZER DOC😄')
-
 st.subheader('CHURNWISE DOC📊')
+st.subheader('EQUITY STOCK CASH MARKET DOC 💵')
+st.subheader('EQUITY STOCK FUTURES & OPIONS DOC 📜')
